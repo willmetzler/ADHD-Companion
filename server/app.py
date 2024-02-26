b155fe8207a7084a44f420b550a48725341656e3
brewchetta <chett.tiller@gmail.com>
2024-02-26 13:08:13 -0500

    changed pipfile versioning for students, added CORS

--- server/app.py
+++ server/app.py
@@ -3,6 +3,7 @@
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_cors import CORS
 
 from models import db # import your models here!
 
@@ -10,6 +11,8 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
+
+CORS(app)
 
 migrate = Migrate(app, db)
 
