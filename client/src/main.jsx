0c1bc98c4f28baa2d563ed836d78f459d7b48bbd
brewchetta <chett.tiller@gmail.com>
2024-05-20 11:51:26 -0400

    remove router starter code

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,20 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App.jsx'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-
-const routes = [
-  {
-    path: '/',
-    element: <App/>,
-    children: []
-  }
-]
-
-const router = createBrowserRouter(routes)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>,
 )
