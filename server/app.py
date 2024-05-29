37274bf8e8dfde4c1382ad48781bb627227584be
brewchetta <chett.tiller@gmail.com>
2024-05-29 09:55:44 -0400

    updated to follow canvas

--- server/app.py
+++ server/app.py
@@ -1,22 +1,8 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from flask_cors import CORS
-
-from models import db # import your models here!
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
-
-CORS(app)
-
-migrate = Migrate(app, db)
-
-db.init_app(app)
+from flask import request, session
+from config import app, db
+from models import ExampleModel # import your models here!
 
 @app.get('/')
 def index():
