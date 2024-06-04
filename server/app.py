d5001dbe36b3fb45279f6f2a6612f698b63d5f3a
Will Metzler <metzlerwill@gmail.com>
2024-06-04 14:33:52 -0400

    initial setup

--- server/app.py
+++ server/app.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python3
+import os
+from flask import Flask, request, session, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_cors import CORS
+from flask_bcrypt import Bcrypt
 
-from flask import request, session
-from config import app, db
-from models import ExampleModel # import your models here!
+from models import db, User, Journal
+
+app = Flask(__name__)
+app.secret_key = os.environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+CORS(app)
+
+bcrypt = Bcrypt(app)
+
+migrate = Migrate(app, db)
+
+db.init_app(app)
 
 @app.get('/')
 def index():
--- server/models.py
+++ server/models.py
@@ -1,7 +1,32 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
 from sqlalchemy.orm import validates
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
-from config import db, bcrypt
+
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+
+db = SQLAlchemy(metadata=metadata)
 
 # write your models here!
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users_table'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False, unique=True)
+    first_name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String, nullable=False)
+    _hashed_password = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+
+class Journal(db.Model, SerializerMixin):
+    __tablename__ = 'journal_table'
+
+    id = db.Column(db.Integer, primary_key=True)
+    journal_header = db.Column(db.String)
+    journal_text = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    mood = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users_table.id'))
