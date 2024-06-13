018e923708dd2db9869733396bc5ff40d293d0dd
Will Metzler <metzlerwill@gmail.com>
2024-06-13 18:26:55 -0400

    sidenav css

--- client/src/components/Sidenav.jsx
+++ client/src/components/Sidenav.jsx
@@ -4,12 +4,26 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faUser, faCalendar, faBook, faPills } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from 'react-router-dom';
 
+
 function Sidenav() {
 
     const navigate = useNavigate();
 
+    const sideNavStyles = {
+        backgroundColor: '#2596be',
+        color: '#fff',
+    };
+    const iconStyle = {
+        fontSize: '1.5em',
+        color: '#fff' 
+    };
+    const textStyle = {
+        color: '#fff'
+    }
+
     return (
         <SideNav
+        style={sideNavStyles}
         onSelect={selected=> {
             console.log(selected)
             navigate('/' + selected)
@@ -19,33 +33,33 @@ function Sidenav() {
             <SideNav.Nav defaultSelected='home'>
                 <NavItem eventKey='home'>
                     <NavIcon>
-                        <FontAwesomeIcon icon={faHome} style={{ fontSize: '1.5em' }} />
+                        <FontAwesomeIcon icon={faHome} style={iconStyle} />
                     </NavIcon>
-                    <NavText>Home</NavText>
+                    <NavText style={textStyle}>Home</NavText>
                 </NavItem>
                 <NavItem eventKey='calendar'>
                     <NavIcon>
-                        <FontAwesomeIcon icon={faCalendar} style={{ fontSize: '1.5em' }} />
+                        <FontAwesomeIcon icon={faCalendar} style={iconStyle} />
                     </NavIcon>
-                    <NavText>Calendar</NavText>
+                    <NavText style={textStyle}>Calendar</NavText>
                 </NavItem>
                 <NavItem eventKey='diary'>
                     <NavIcon>
-                        <FontAwesomeIcon icon={faBook} style={{ fontSize: '1.5em' }} />
+                        <FontAwesomeIcon icon={faBook} style={iconStyle} />
                     </NavIcon>
-                    <NavText>Diary</NavText>
+                    <NavText style={textStyle}>Diary</NavText>
                 </NavItem>
                 <NavItem eventKey='medications'>
                     <NavIcon>
-                        <FontAwesomeIcon icon={faPills} style={{ fontSize: '1.5em' }} />
+                        <FontAwesomeIcon icon={faPills} style={iconStyle} />
                     </NavIcon>
-                    <NavText>Medications</NavText>
+                    <NavText style={textStyle}>Medications</NavText>
                 </NavItem>
                 <NavItem eventKey='profile'>
                     <NavIcon>
-                        <FontAwesomeIcon icon={faUser} style={{ fontSize: '1.5em' }} />
+                        <FontAwesomeIcon icon={faUser} style={iconStyle} />
                     </NavIcon>
-                    <NavText>Profile</NavText>
+                    <NavText style={textStyle}>Profile</NavText>
                 </NavItem>
             </SideNav.Nav>
         </SideNav>
